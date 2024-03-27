7d57272e18021af422a04ba7d5542585e628baeb
Bhavya <bilimoriabhavya1@gmail.com>
2024-03-27 12:54:19 +0530

    Completed the project

--- Driver.py
+++ Driver.py
@@ -5,33 +5,33 @@ from Decryptor import Decrypt as de
 choice=input("Enter your choice (E for Encryption/D for Decryption): ")
 if choice=="E" or choice == "e":
     while True:
-        fname=input("Enter the name of the file:")
-        if os.path.exists(fname):
+        fileName=input("Enter the name of the file:")
+        if os.path.exists(fileName):
             break
-        elif os.path.exists(f"{fname}.txt"):
-            fname=f"{fname}.txt"
+        elif os.path.exists(f"{fileName}.txt"):
+            fileName=f"{fileName}.txt"
             break
         else:
             print("Entered name of file does not exists.Enter another name.")
             continue
         
     code=(int)(input("Enter a 3 digit code for the file: "))
-    enObj=en(fname,code)
+    enObj=en(fileName,code)
     enObj.encrypt()
     del enObj
 
 if choice=="D" or choice=="d":
     while True:
-        fname=input("Enter the name of the file:")
-        if os.path.exists(fname):
+        fileName=input("Enter the name of the file:")
+        if os.path.exists(fileName):
             break
-        elif os.path.exists(f"{fname}.txt"):
-            fname=f"{fname}.txt"
+        elif os.path.exists(f"{fileName}.txt"):
+            fileName=f"{fileName}.txt"
             break
         else:
             print("Entered name of file does not exists.Enter another name.")
             continue
     code=(int)(input("Enter a 3 digit code for the file: "))
-    deObj=de(fname,code)
+    deObj=de(fileName,code)
     deObj.decrypt()
     del deObj
